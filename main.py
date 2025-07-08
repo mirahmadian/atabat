6f68674c285b29090df214605d7a3cfcee7c09f2
mirahmadian <153491033+mirahmadian@users.noreply.github.com>
2025-07-08 07:31:16 +0330

    Create main.py

---
+++ main.py
@@ -0,0 +1,11 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/')
+def hello():
+    return 'سلام از عتبات! برنامه شما روی لیارا اجرا شده است.'
+
+if __name__ == '__main__':
+    app.run()
+  
