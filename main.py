6d59af52ca6333da942f53645e006b67053f9a51
mirahmadian <153491033+mirahmadian@users.noreply.github.com>
2025-07-10 20:51:42 +0330

    Update main.py

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ import datetime
 # --- تنظیمات اولیه ---
 app = Flask(__name__)
 DB_PATH = "atabat_sample.db"
-BOT_TOKEN = "YOUR_BOT_TOKEN" # <<< توکن بات خود را اینجا قرار دهید
+BOT_TOKEN = "6616020:CAwP1U9uX7ibGLXM17Cb9BztVy97pZUUXnDWvIjX" # <<< توکن بات خود را اینجا قرار دهید
 
 # ... (بقیه کد شما تا این بخش بدون تغییر باقی می‌ماند) ...
 user_states = {}
